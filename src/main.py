9c0624822440c78aa3e857bc759966cbf5af7c65
James Yu <pengyan@pengyandeMacBook-Air-9.local>
2026-06-27 22:55:54 +0800

    add again function

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ def greet(name: str) -> str:
 def farewell(name: str) -> str:
     return f"Goodbye, {name}!"
 
+def seeyouagain(name:str) ->str:
+    return f"again, {name}!"
 
 if __name__ == "__main__":
     print(greet("Git"))
     print(farewell("Git"))
+    print(seeyouagain("Git")
